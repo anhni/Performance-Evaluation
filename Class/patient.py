1a36777aa940ed40a37e76ec188a23e8a3bf4894
Anh Ni <anhni220502002@gmail.com>
2023-11-28 21:45:33 +0700

    update time for system

--- Class/patient.py
+++ Class/patient.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 import simpy
+import statistics
 
 class Patient ():
     
@@ -8,7 +9,12 @@ class Patient ():
         self.env = env
         self.name = name
         self.ticketNumber = 0
-        self.joinclinical = False
+
+        self.joinClinical = False
+
+        self.joinPharmacy = False
+        self.joinTesting = False
+        self.joinSurgery = False
 
         self.leaveSystem = False
 
@@ -18,110 +24,218 @@ class Patient ():
         yield env.process(system.run(self))
         
 class System():
-    def __init__(self, env, bookingqueue, notbookingqueue, clinicalServer) -> None:
+    def __init__(self, env, bookingqueue, notbookingqueue, clinicalServer, pharmacyServer, testingServer, sugeryServer) -> None:
         self.env = env
+
+        # Server
         self.bookingqueue = bookingqueue
         self.notbookingqueue = notbookingqueue
         self.clinicalServer = clinicalServer
-        self.ticketNumber = 0
-    
+        self.pharmacyServer = pharmacyServer
+        self.testingServer = testingServer
+        self.sugeryServer = sugeryServer
+
+        # Waiting time
+        self.WT_bookingqueue =[] 
+        self.WT_notbookingqueue =[]
+        self.WT_clinicalServer =[]
+        self.WT_pharmacyServer =[]
+        self.WT_sugeryServer =[]
+        self.WT_testingServer =[]
+
+        # Service time
+        self.ST_bookingqueue =[] 
+        self.ST_notbookingqueue =[]
+        self.ST_clinicalServer =[]
+        self.ST_pharmacyServer =[]
+        self.ST_sugeryServer =[]
+        self.ST_testingServer =[]
+
+        # Number customer join server
+        self.NC_bookingqueue = 0
+        self.NC_notbookingqueue = 0
+        self.NC_clinicalServer = 0
+        self.NC_pharmacyServer = 0
+        self.NC_sugeryServer = 0
+        self.NC_testingServer = 0
+        
+        # Ticket number
+        self.ticketNumber = 0  
 
     def run(self, patient):
         if random.random() > 0.3:    
-            yield self.env.process(self.notBookingQueue(self.env, patient))
+            yield self.env.process(self.notBookingQueue(self.env, patient, 5))
         else: 
-            yield self.env.process(self.bookingQueue(self.env, patient)) 
+            yield self.env.process(self.bookingQueue(self.env, patient, 4)) 
 
         while(not patient.leaveSystem):
-            yield self.env.process(self.clinicalExamination(self.env, patient)) 
-        
-
-        
-
-    
+            if(patient.joinClinical):
+                yield self.env.process(self.clinicalExamination(self.env, patient, 2)) 
+            if(patient.joinPharmacy):
+                yield self.env.process(self.pharmacyArea(self.env, patient, 3)) 
+            if(patient.joinTesting):
+                yield self.env.process(self.TestingRoom(self.env, patient, 4))
+            if(patient.joinSurgery):
+                yield self.env.process(self.SurgeryRoom(self.env, patient, 2)) 
+             
     def getTicket(self, patient):
         patient.ticketNumber = self.ticketNumber
         self.ticketNumber += 1 
         
-    def bookingQueue(self, env, patient):
+    def bookingQueue(self, env, patient, timeService):
         # customer arrives to the system, waits and leaves
         arrive = env.now
-        service_time = random.expovariate(0.5)
-        maxWaitingTimeOfCustomer = np.random.poisson(3, size=None)
+        service_time = random.expovariate(timeService)
+        # maxWaitingTimeOfCustomer = np.random.poisson(3, size=None)
 
         with self.bookingqueue.request() as req:      
-            results = yield req | env.timeout(maxWaitingTimeOfCustomer)
+            # results = yield req | env.timeout(maxWaitingTimeOfCustomer)
+                yield req
+                self.NC_bookingqueue += 1 
 
-            if req in results:
+            # if req in results:
                 self.getTicket(patient)
                 print('%7.4f : %s join booking server[%i/%i] after wait %7.4f' % (env.now, patient.name, self.bookingqueue.count, self.bookingqueue.capacity, env.now-arrive))
+                self.WT_bookingqueue.append(env.now-arrive)
                 servertime = service_time
                 yield env.timeout(servertime)
-                # serviceTimes.append(servertime)
+                self.ST_bookingqueue.append(servertime)
                 print('%7.4f : %s leave booking server after %7.4f with number ticket %i' % (env.now, patient.name, servertime, patient.ticketNumber))
                 patient.joinClinical = True
-            else:
-                waiting_time = env.now - arrive
-                # waitingTimes.append(waiting_time)
-                print('%s Waiting %6.3f at booking queue then left' % (patient.name, waiting_time))
-                patient.leaveSystem = True
+            # else:
+            #     waiting_time = env.now - arrive
+            #     # waitingTimes.append(waiting_time)
+            #     print('%s Waiting %6.3f at booking queue then left' % (patient.name, waiting_time))
+            #     patient.leaveSystem = True
     
-    def notBookingQueue(self, env, patient):
+    def notBookingQueue(self, env, patient, timeService):
         # customer arrives to the system, waits and leaves
         arrive = env.now
-        service_time = random.expovariate(0.3)
-        maxWaitingTimeOfCustomer = np.random.poisson(3, size=None)
+        service_time = random.expovariate(timeService)
+        # maxWaitingTimeOfCustomer = np.random.poisson(3, size=None)
 
         with self.notbookingqueue.request() as req:        
-            results = yield req | env.timeout(maxWaitingTimeOfCustomer)
-
-            if req in results:
+            # results = yield req | env.timeout(maxWaitingTimeOfCustomer)
+                yield req
+                self.NC_notbookingqueue += 1
+            # if req in results:
                 print('%7.4f : %s join not booking server[%i/%i] after wait %7.4f' % (env.now, patient.name, self.notbookingqueue.count, self.notbookingqueue.capacity, env.now-arrive))
+                self.WT_notbookingqueue.append(env.now-arrive)
                 servertime = service_time
                 yield env.timeout(servertime)
-                # serviceTimes.append(servertime)
+                self.ST_notbookingqueue.append(servertime)
                 self.getTicket(patient)
                 print('%7.4f : %s leave not booking server after %7.4f with number ticket %i' % (env.now, patient.name, servertime, patient.ticketNumber))
                 patient.joinClinical = True
-            else:
-                waiting_time = env.now - arrive
-                # waitingTimes.append(waiting_time)
-                print('%s Waiting %6.3f at not booking queue then left' % (patient.name, waiting_time))
-                patient.leaveSystem = True
-
-    
-    # def clinicalExamination(self, env, patient):
-    #     arrive = env.now
-    #     service_time = random.expovariate(0.3)
-    #     maxWaitingTimeOfCustomer = np.random.poisson(4, size=None)
-
-    #     with self.notbookingqueue.request(priority = patient.ticketNumber) as req:        
-    #         results = yield req | env.timeout(maxWaitingTimeOfCustomer)
-
-    #         if req in results:
-    #             print('%7.4f : Get in clinical examination Time of %s' % (env.now, patient.name))
-    #             servertime = service_time
-    #             yield env.timeout(servertime)
-    #             # serviceTimes.append(servertime)
-    #             self.getTicket(patient)
-    #             print('%7.4f : Get out clinical examination Time of %s with number ticket %i' % (env.now, patient.name, patient.ticketNumber))
-    #             # print('%7.4f Time Spent in the queue of %s' % (env.now - arrive, patient.name))
-    #         else:
-    #             waiting_time = env.now - arrive
-    #             # waitingTimes.append(waiting_time)
-    #             print('%6.3f : Waiting time at clinical examination then left of %s' % (waiting_time, patient.name))
-    
-    def clinicalExamination(self, env, patient):
+            # else:
+            #     waiting_time = env.now - arrive
+            #     # waitingTimes.append(waiting_time)
+            #     print('%s Waiting %6.3f at not booking queue then left' % (patient.name, waiting_time))
+            #     patient.leaveSystem = True
+   
+    def clinicalExamination(self, env, patient, timeService):
         arrive = env.now
-        service_time = random.expovariate(1.0 / (np.random.poisson(10, size=None) + 0.01))
+        service_time = random.expovariate(timeService)
         # maxWaitingTimeOfCustomer = np.random.poisson(4, size=None)
 
-        with self.clinicalServer.request(patient.ticketNumber, False) as req:        
+        with self.clinicalServer.request(patient.ticketNumber, False) as req:       
+            yield req 
+            self.NC_clinicalServer += 1
             print('%7.4f : %s join clinical server[%i/%i] after wait %7.4f with number ticket %i' % (env.now, patient.name, self.clinicalServer.count, self.clinicalServer.capacity, env.now-arrive, patient.ticketNumber))
+            self.WT_clinicalServer.append(env.now-arrive)
             servertime = service_time
             yield env.timeout(servertime)
-            # serviceTimes.append(servertime)
+            self.ST_clinicalServer.append(servertime)
             print('%7.4f : %s leave clinical server after %7.4f' % (env.now, patient.name, servertime))
             # print('%7.4f Time Spent in the queue of %s' % (env.now - arrive, patient.name))
+            
+            ratio = random.random()
+            if ratio < 0.4:    
+                patient.joinPharmacy = True
+            elif ratio > 0.6: 
+                patient.joinSurgery = True
+            else:
+                patient.joinTesting = True
+                          
+    def pharmacyArea(self, env, patient, timeService):
+        arrive = env.now
+        service_time = random.expovariate(timeService)
+        # maxWaitingTimeOfCustomer = np.random.poisson(4, size=None)
+
+        with self.pharmacyServer.request() as req:     
+            yield req 
+            self.NC_pharmacyServer += 1  
+            print('%7.4f : %s join pharmacy server[%i/%i] after wait %7.4f with number ticket %i' % (env.now, patient.name, self.pharmacyServer.count, self.pharmacyServer.capacity, env.now-arrive, patient.ticketNumber))
+            self.WT_pharmacyServer.append(env.now-arrive)
+            servertime = service_time
+            yield env.timeout(servertime)
+            self.ST_pharmacyServer.append(servertime)
+            print('%7.4f : %s leave pharmacy server after %7.4f' % (env.now, patient.name, servertime))
+            # print('%7.4f Time Spent in the queue of %s' % (env.now - arrive, patient.name))
             patient.leaveSystem = True
+    
+    def SurgeryRoom(self, env, patient, timeService):
+        arrive = env.now
+        service_time = random.expovariate(timeService)
+        # maxWaitingTimeOfCustomer = np.random.poisson(4, size=None)
+
+        with self.sugeryServer.request() as req: 
+            yield req     
+            self.NC_sugeryServer += 1  
+            print('%7.4f : %s join surgery room server[%i/%i] after wait %7.4f with number ticket %i' % (env.now, patient.name, self.sugeryServer.count, self.sugeryServer.capacity, env.now-arrive, patient.ticketNumber))
+            self.WT_sugeryServer.append(env.now-arrive)
+            servertime = service_time
+            yield env.timeout(servertime)
+            self.ST_sugeryServer.append(servertime)
+            print('%7.4f : %s leave surgery room after %7.4f' % (env.now, patient.name, servertime))
+            # print('%7.4f Time Spent in the queue of %s' % (env.now - arrive, patient.name))
+            
+            ratio = random.random()
+            if ratio < 0.8:    
+                patient.joinPharmacy = True
+            else:
+                patient.leaveSystem = True
+
+    def TestingRoom(self, env, patient, timeService):
+        arrive = env.now
+        service_time = random.expovariate(timeService)
+        # maxWaitingTimeOfCustomer = np.random.poisson(4, size=None)
+
+        with self.testingServer.request() as req:  
+            yield req    
+            self.NC_testingServer += 1   
+            print('%7.4f : %s join testing room server[%i/%i] after wait %7.4f with number ticket %i' % (env.now, patient.name, self.testingServer.count, self.testingServer.capacity, env.now-arrive, patient.ticketNumber))
+            self.WT_pharmacyServer.append(env.now-arrive)
+            servertime = service_time
+            yield env.timeout(servertime)
+            self.ST_testingServer.append(servertime)
+            print('%7.4f : %s leave testing room after %7.4f' % (env.now, patient.name, servertime))
+            # print('%7.4f Time Spent in the queue of %s' % (env.now - arrive, patient.name))
+            
+            ratio = random.random()
+            if ratio < 0.6:    
+                patient.joinPharmacy = True
+            else:
+                patient.leaveSystem = True
+
+    def calculatorTime(self, waitingTimes, serviceTimes , numberCustomers):
+        # interarrivalTimes.append(interarrival)
+        # average_interarrival = statistics.mean(interarrivalTimes)
+        if len(serviceTimes) > 0:
+            average_serviceTime = statistics.mean(serviceTimes)
+            # print("Average Interarrival Time Is : %7.4f" % average_interarrival)
+            print("Average Service Time Is : %7.4f" % average_serviceTime)
+
+        if len(waitingTimes) > 0:
+            average_waitingTime = statistics.mean(waitingTimes)
+            print("Average Waiting Time Is : %7.4f" % average_waitingTime)
+        
+        print("Number of customer joins server Is : %7.4f" % numberCustomers)
+
+    
+        
+
+
+    
+
                 
--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import simpy
 import numpy as np
 from random import seed
 from Class.patient import *
+import statistics
+
 
 seed(29343)  # for seed of randint function
 random_seed = 32  # for seed of other random generators
-new_customers = 30  # Total number of customers in the system
-interarrival = np.random.poisson(3, size=None)  # Generate new customers roughly every x seconds
+new_customers = 3  # Total number of customers in the system
+interarrival = np.random.poisson(0.1, size=None)  # Generate new customers roughly every x seconds
 maxWaitingTimeOfCustomer = np.random.poisson(4, size=None)
 patientNumber = 0
 waitingTimes = []
@@ -16,18 +18,43 @@ interarrivalTimes = []
 
 def generator(env, number, interval):  # customer generator with interarrival times.
     """generator generates customers randomly"""
-    newSystem = System(env1, bookingServer, notBookingServer, clinicalServer)
     for i in range(number):
         newPatient = Patient(env1, 'Customer %02d' % i)
         env.process(newPatient.joinSystem(env1, newSystem))
         t = random.expovariate(1.0 / (interval + 0.01))
         yield env.timeout(t)  # adds time to the counter, does not delete from the memory
+    
 
 random.seed(random_seed)
 env1 = simpy.Environment()
 bookingServer = simpy.Resource(env1, capacity=1)  # capacity changes the number of generators in the system.
 notBookingServer = simpy.Resource(env1, capacity=2)
 clinicalServer = simpy.PriorityResource(env1, capacity=6)
+pharmacyServer = simpy.Resource(env1, capacity=3)
+specializedServer = simpy.Resource(env1, capacity=4)
+testingServer = simpy.Resource(env1, capacity=2)
+
+newSystem = System(env1, bookingServer, notBookingServer, clinicalServer, pharmacyServer, specializedServer, testingServer)
+
 
 env1.process(generator(env1, new_customers, interarrival))
 env1.run()
+
+print("----booking server----")
+newSystem.calculatorTime(newSystem.WT_bookingqueue, newSystem.ST_bookingqueue, newSystem.NC_bookingqueue)
+
+print("----not booking server----")
+newSystem.calculatorTime(newSystem.WT_notbookingqueue, newSystem.ST_notbookingqueue, newSystem.NC_notbookingqueue)
+
+print("----clinical server----")
+newSystem.calculatorTime(newSystem.WT_clinicalServer, newSystem.ST_clinicalServer, newSystem.NC_clinicalServer)
+
+print("----pharmacy area server----")
+newSystem.calculatorTime(newSystem.WT_pharmacyServer, newSystem.ST_pharmacyServer, newSystem.NC_pharmacyServer)
+
+print("----minor surgery server----")
+newSystem.calculatorTime(newSystem.WT_sugeryServer, newSystem.ST_sugeryServer, newSystem.NC_sugeryServer)
+
+print("----testing room server----")
+newSystem.calculatorTime(newSystem.WT_testingServer, newSystem.ST_testingServer, newSystem.NC_testingServer)
+
